fdfb292d47d43b31a9f286a8f4854b549d14e066
ceyedev <100298860+ceyedev@users.noreply.github.com>
2026-08-31 18:57:28 +0200

    Update script source URL to raw GitHub link

--- ceyectify_with_updates.js
+++ ceyectify_with_updates.js
@@ -3,7 +3,7 @@
     script.setAttribute("type", "text/javascript");
     script.setAttribute(
       "src",
-      "https://github.com/ceyedev/ceyecetify/ceyectify_without_updates.js"
+      "https://raw.githubusercontent.com/ceyedev/ceyecetify/refs/heads/main/ceyectify_without_updates.js"
     );
     document.head.appendChild(script);
   })();
