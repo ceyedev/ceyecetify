fd00b9177ff0a28a82fa85359cd7a90d7549d755
ceyedev <100298860+ceyedev@users.noreply.github.com>
2026-08-31 22:01:13 +0200

    Add files via upload

--- uninstall.ps1
+++ uninstall.ps1
@@ -16,18 +16,30 @@ Write-Host ""
 $uninstall = Read-Host "Proceed with the uninstallation? [Y/n]"
 
 if ($uninstall -ine "n") {
+    $needapply = $false
+
     if (Test-Path ".\ceyectify_without_updates.js") {
         Write-Host "Remove 'ceyectify_without_updates.js'..."
         Remove-Item ".\ceyectify_without_updates.js"
         spicetify config extensions ceyectify_without_updates.js-
+        $needapply = $true
     }
 
     if (Test-Path ".\ceyectify_with_updates.js") {
         Write-Host "Remove 'ceyectify_with_updates.js'..."
         Remove-Item ".\ceyectify_with_updates.js"
         spicetify config extensions ceyectify_with_updates.js-
+        $needapply = $true
+    }
+
+    if ($needapply) {
+        spicetify apply
     }
 }
+
+Write-Host ""
+Write-Host "Remaining Extensions:"
+Get-ChildItem
 
 Write-Host ""
 Write-Host "You're done, enjoy the normal Spotify"
