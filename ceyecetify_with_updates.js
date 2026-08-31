68e133e9c477cb6d960c168f0a374949a2f6c911
ceyedev <100298860+ceyedev@users.noreply.github.com>
2026-09-01 01:14:06 +0200

    fixed space issue

--- ceyecetify_with_updates.js
+++
@@ -1,5 +0,0 @@
-(async () => {
-    const response = await fetch("https://raw.githubusercontent.com/ceyedev/ceyecetify/refs/heads/main/ceyecetify_without_updates.js");
-    const code = await response.text();
-    eval(code);
-})();
